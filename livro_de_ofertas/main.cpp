a7dd024e135dfe9cc7ef644a4131d6c675e25ca6
Raph2ll <raphael.ph2ll@gmail.com>
2024-01-15 17:09:29 -0300

    refactor: desafio2

--- livro_de_ofertas/main.cpp
+++ livro_de_ofertas/main.cpp
@@ -1,169 +1,204 @@
 #include <iostream>
 #include <string>
-#include <map>
-#include <fstream>
-#include <sstream>
 #include <vector>
+#include <sstream>
+
 using namespace std;
 
-// Verifique se o primeiro elemento é igual a "acao"
-bool verificarRepeticao(const string& acao) {
-    ifstream leituraArquivo("ofertas.txt");
-    string linha;
-    
-    while (getline(leituraArquivo, linha)) {
-        istringstream iss(linha);
-        vector<string> elementos;
+struct Oferta {
+    string posicao;
+    double valor;
+    int quantidade;
+};
 
-        // Use ',' como delimitador para obter todos os elementos
-        while (getline(iss, linha, ',')) {
-            elementos.push_back(linha);
-        }
+vector<Oferta> ofertas;
 
-        // Verifique se o primeiro elemento é igual a "acao"
-        if (!elementos.empty() && elementos[0] == acao) {
-            leituraArquivo.close();
-            return true; // Encontrado, é repetido
+bool verificarRepeticao(const string& posicao) {
+    for (const Oferta& oferta : ofertas) {
+        if (oferta.posicao == posicao) {
+            return true;
         }
     }
-
-    leituraArquivo.close();
     return false;
 }
 
-void inserir(string acao) {
-
-    if (verificarRepeticao(acao)) {
+void inserir(string posicao) {
+    if (verificarRepeticao(posicao)) {
         cout << "Ação repetida. Não será inserida novamente." << endl;
+        return; 
+    }
+
+    Oferta novaOferta;
+    novaOferta.posicao = posicao;
+
+    cout << "Digite o valor da sua oferta. [0.0]" << endl;
+
+    while (true) {
+        try {
+            string userInput;
+            getline(cin, userInput);
+            novaOferta.valor = stod(userInput);
+            if (novaOferta.valor < 0.0) {
+                cout << "Digite um valor maior que: 0.0" << endl;
+            } else {
+                break;
+            }
+        } catch (const invalid_argument& e) {
+            cerr << "Erro de conversão: \nDigite um valor válido." << endl;
+        }
+    }
+
+    cout << "Digite a quantidade da oferta. [0]" << endl;
+
+    while (true) {
+        try {
+            string userInput;
+            getline(cin, userInput);
+            novaOferta.quantidade = stoi(userInput);
+            if (novaOferta.quantidade < 0) {
+                cout << "Digite um valor maior que: 0" << endl;
+            } else {
+                break;
+            }
+        } catch (const invalid_argument& e) {
+            cerr << "Erro de conversão: \nDigite um valor válido." << endl;
+        }
+    }
+
+    ofertas.push_back(novaOferta);
+}
+void modificar(const string& posicao) {
+    double novoValor;
+    int novaQuantidade;
+
+    cout << "Digite o valor da sua oferta. [0.0]" << endl;
+
+    while (true) {
+        try {
+            string userInput;
+            getline(cin, userInput);
+            novoValor = stod(userInput);
+            if (novoValor < 0) {
+                cout << "Digite um valor maior que: 0" << endl;
+            } else {
+                break;
+            }
+        } catch (const invalid_argument& e) {
+            cerr << "Erro de conversão: \nDigite um valor válido." << endl;
+        }
+    }
+
+    cout << "Digite a quantidade da oferta. [0]" << endl;
+
+    while (true) {
+        try {
+            string userInput;
+            getline(cin, userInput);
+            novaQuantidade = stoi(userInput);
+            if (novaQuantidade < 0) {
+                cout << "Digite um valor maior que: 0" << endl;
+            } else {
+                break;
+            }
+        } catch (const invalid_argument& e) {
+            cerr << "Erro de conversão: \nDigite um valor válido." << endl;
+        }
+    }
+
+    for (auto it = ofertas.begin(); it != ofertas.end(); ++it) {
+        if (it->posicao == posicao) {
+            it->valor = novoValor;
+            it->quantidade = novaQuantidade;
+            cout << "Oferta com posição " << posicao << " modificada com sucesso." << endl;
+            return;
+        }
+    }
+
+    cout << "Oferta com posição " << posicao << " não encontrada." << endl;
+}
+void deletar(string posicao) {
+    if (!verificarRepeticao(posicao)) {
+        cout << "Valor não encontrado." << endl;
+        return; 
+    }
+    for (auto it = ofertas.begin(); it != ofertas.end();) {
+        if (it->posicao == posicao) {
+            it = ofertas.erase(it); // Após a remoção, `it` é automaticamente ajustado
+            cout << "Oferta com ação " << posicao << " deletada com sucesso." << endl;
+        } else {
+            ++it;
+        }
+    }
+
+    if (ofertas.empty()) {
+        cout << "Lista de ofertas vazia." << endl;
+    }
+}
+
+void imprimirOfertas() {
+    if (ofertas.empty()) {
+        cout << "Lista de ofertas vazia." << endl;
         return;
     }
 
-    ofstream arquivo("ofertas.txt", ios::app);
-    string userInput;
-    double valor;
-    string ret = acao + ",";
-    int quantidade;
-
-    cout << "Digite o valor da sua oferta. [0.0]\n" << endl;
-
-    while (true) {
-        try {
-            getline(cin, userInput);
-            valor = stod(userInput);
-            if(valor < 0.0){
-                cout << "Digite um valor maior que: 0.0\n" << endl;
-            }   
-                ret += to_string(valor) + ",";
-                break;
-        } catch (const invalid_argument& e) {
-            cerr << "Erro de conversão: " << "\nDigite um valor válido.\n" << endl;
-        }
-    }
-
-    cout << "Digite a quantidade da oferta. [0]\n" << endl;
-
-    while (true) {
-        try {
-            getline(cin, userInput);
-            quantidade = stoi(userInput);
-            if (quantidade < 0) {
-                cout << "Digite um valor maior que: 0\n" << endl;
-            }
-                ret += to_string(quantidade) + ",";
-                break;
-        } catch (const invalid_argument& e) {
-            cerr << "Erro de conversão: " << "\nDigite um valor válido.\n" << endl;
-        }
-        
-    }
-    arquivo << ret << endl;
-    
-}
-void modificar() {
-
-}
-void deletar() {
-
-}
-
-void encerrar() {
-
-}
-
-bool arquivoExiste(const string& nomeArquivo) {
-    ifstream arquivo(nomeArquivo.c_str());
-    return arquivo.good();
-}
-
-void criarOuAbrirArquivo(ofstream& arquivo, const string& nomeArquivo) {
-    if (arquivoExiste(nomeArquivo)) {
-        cout << "O arquivo já existe. Continuando o programa." << endl;
-    } else {
-        cout << "O arquivo não existe. Criando um novo arquivo." << endl;
-        arquivo.open(nomeArquivo.c_str());
-        arquivo.close();
-        cout << "Novo arquivo criado com sucesso." << endl;
+    cout << "Lista de ofertas:" << endl;
+    for (const Oferta& oferta : ofertas) {
+        cout << oferta.posicao << "," << oferta.valor << "," << oferta.quantidade << endl;
     }
 }
-
 int main() {
-
-    string nomeArquivo = "ofertas.txt";
-    ofstream meuArquivo;
-
-    criarOuAbrirArquivo(meuArquivo, nomeArquivo);
-
-    cout << "Bem-vindo ao livro de ofertas.\n" << endl;
+    cout << "Bem-vindo ao livro de ofertas." << endl;
+    
     while (true) {
         int numero = 0;
         string ret = "";
         string userInput = "";
+
         while (true) {
-            cout << "Insira o número da oferta:\n" << endl;
+            cout << "Insira o número da oferta:" << endl;
+
             try {
                 getline(cin, userInput);
                 numero = stoi(userInput);
 
                 if (numero < 0) {
-                    cout << "Digite um valor maior que 0.\n" << endl;
+                    cout << "Digite um valor maior que 0." << endl;
                 } else {
                     ret += to_string(numero);
                     break;
                 }
             } catch (const invalid_argument& e) {
-                cerr << "Erro de conversão: " << "\nDigite um valor válido.\n" << endl;
+                cerr << "Erro de conversão: \nDigite um valor válido." << endl;
             }
         }
+
         while (true) {
             cout << "Selecione uma das opções abaixo:\n"
-                << "0: Inserir\n"
-                << "1: Modificar\n"
-                << "2: Deletar\n"
-                << "3: Encerrar\n"
-                << endl;
+                 << "0: Inserir\n"
+                 << "1: Modificar\n"
+                 << "2: Deletar\n"
+                 << "3: Ofertas\n" << endl;
 
             getline(cin, userInput);
-
-            cout << "Você digitou: " << userInput << "\n" << endl;
+            // cout << "Você digitou: " << userInput << "\n" << endl;
 
             if (userInput == "0") {
                 inserir(ret);
                 break;
             } else if (userInput == "1") {
-                modificar();
+                modificar(ret);
+                break;
             } else if (userInput == "2") {
-                deletar();
+                deletar(ret);
+                break;
             } else if (userInput == "3") {
-                encerrar();
+                imprimirOfertas();
+                break;
             } else {
-                cout << "Digite um valor válido entre [0,1,2,3].\n" << endl;
+                cout << "Digite um valor válido entre [0, 1, 2, 3]." << endl;
             }
         }
     }
-    
-
-    
-
 
     return 0;
 }
